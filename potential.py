78c57aa30a45f056bf6a524061421044be8a802d
fabiobrau <fabio.br92@yahoo.it>
2020-03-12 19:56:01 +0100

    Add fitness

--- potential.py
+++ potential.py
@@ -1,7 +1,8 @@
 '''
     Computes potentials and fitness
 '''
-from data_models import Resource
+from data_models import Resource, ProblemInstance
+import numpy as np
 
 
 def total_potential(r_1: Resource, r_2: Resource):
@@ -15,3 +16,36 @@ def total_potential(r_1: Resource, r_2: Resource):
     work_pot = inter_size*(union_size - inter_size)
     bonus_pot = (r_1.bonus * r_2.bonus)*(r_1.company == r_2.company)
     return work_pot+bonus_pot
+
+
+def fitness(prob_inst, dev_loc, man_loc):
+    # Change to list of tuple, each tuple is a coordinate in the map
+    all = dev_loc + man_loc
+    all_loc_dict = {loc: i for i, loc in enumerate(all)}
+    all_couple_loc = set()
+    for loc in all:
+        # Take only not empty tumples
+        if loc == ():
+            continue
+        # nearest positions
+        loc_t = (loc[0], loc[1]+1)
+        loc_b = (loc[0], loc[1]-1)
+        loc_r = (loc[0]+1, loc[1])
+        loc_l = (loc[0]-1, loc[1])
+        # update only if exist
+        if loc_t in all:
+            all_couple_loc.update({loc, loc_t})
+        if loc_b in all:
+            all_couple_loc.update({loc, loc_b})
+        if loc_r in all:
+            all_couple_loc.update({loc, loc_r})
+        if loc_l in all:
+            all_couple_loc.update({loc, loc_l})
+    # change set of set in list of tuple
+    all_couple_list = list(tuple(c) for c in all_couple_loc)
+    all_r = prob_inst.developer_list + prob_inst.mangers_list
+    all_couple_r = [[all_r[all_loc_dict[x]], all_r[all_loc_dict[y]]] for x, y in all_couple_list]
+    return sum(total_potential(couple_r[0], couple_r[1]) for couple_r in all_couple_r)
+
+
+
